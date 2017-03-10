37c24f711301172a50898088304adc21ff49d703
Justin <jcolethekid@gmail.com>
2017-03-10 06:07:02 -0500

    Removed Accounts Framework Testing

--- JustinColeman_1703/PerspecTV/PerspecTV/LoginViewController.swift
+++ JustinColeman_1703/PerspecTV/PerspecTV/LoginViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import SafariServices
 import OAuthSwift
-import Accounts
 
 class LoginViewController: UIViewController, UIWebViewDelegate {
 
@@ -24,10 +23,6 @@ class LoginViewController: UIViewController, UIWebViewDelegate {
     var currentUser: User!
     var userLoggedIn: Bool!
     
-    //MARK: Accounts Test
-    var accounts = [Any]()
-    var accountStore: ACAccountStore!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,31 +31,7 @@ class LoginViewController: UIViewController, UIWebViewDelegate {
         
         loginBtn.layer.cornerRadius = 6
         
-        //MARK: Accounts Test
-        accountStore = ACAccountStore()
-        let accountType: ACAccountType = accountStore!.accountType(withAccountTypeIdentifier: "IdentifierTwitch")
-        accountStore.requestAccessToAccounts(with: accountType, options: nil) { (granted, error) in
-            if granted{
-                //Access account store and retrieves all accounts with this type
-                self.accounts = self.accountStore!.accounts(with: accountType)
-                
-                //Check if the user has any stored accounts
-                if self.accounts.count == 0{
-                    self.twitchAuth()
-                }else{
-                    //Load the account's data
-                    let account = self.accounts.first as! ACAccount
-                    self.currentUser = User(authToken: account.credential.oauthToken)
-                    self.getUser()
-                }
-            }
-            
-            if error != nil{
-                print(error?.localizedDescription ?? "Mistakes were made")
-            }
-        }
-        
-        //load()
+        load()
     }
 
     override func didReceiveMemoryWarning() {
@@ -70,7 +41,7 @@ class LoginViewController: UIViewController, UIWebViewDelegate {
     
     @IBAction func twitchAuth(){
         oauth.authorizeURLHandler = SafariURLHandler(viewController: self, oauthSwift: oauth)
-        let _ = oauth.authorize(withCallbackURL: appDelegate.redirectUrl, scope: "user_read+chat_login+user_subscriptions", state: generateState(withLength: 6), success: { (Credential, response, params) in
+        let _ = oauth.authorize(withCallbackURL: appDelegate.redirectUrl, scope: "user_read+chat_login+user_subscriptions", state: generateState(withLength: 100), success: { (Credential, response, params) in
             self.currentUser = User(authToken: Credential.oauthToken)
             self.getUser()
         }, failure: nil)
@@ -81,28 +52,10 @@ class LoginViewController: UIViewController, UIWebViewDelegate {
         downloadandParse(urlString: "https://api.twitch.tv/kraken/user?oauth_token=\(currentUser.authToken)&client_id=\(appDelegate.consumerID)&\(appDelegate.apiVersion)")
     }
     
-    /*func save(){
+    func save(){
         UserDefaults.standard.set(true, forKey: "LoggedIn")
         UserDefaults.standard.set(currentUser.authToken, forKey: "AuthToken")
         UserDefaults.standard.set(URL(string: "https://api.twitch.tv/kraken/users/\(currentUser.id)?oauth_token=\(currentUser.authToken)&client_id=\(appDelegate.consumerID)&\(appDelegate.apiVersion)") , forKey: "UserLink")
-    }*/
-    
-    func save(){
-        let accountType: ACAccountType = accountStore!.accountType(withAccountTypeIdentifier: "Twitch")
-        let account = ACAccount(accountType: accountType)
-        account?.credential = ACAccountCredential(oAuthToken: appDelegate.consumerID, tokenSecret: appDelegate.secret)
-        account?.username = currentUser.username
-        
-        accountStore.saveAccount(account) { (Saved, error) in
-            if Saved{
-                print("Saved Twitch Account")
-            }
-            
-            if error != nil{
-                print(error?.localizedDescription ?? "Mistakes were made")
-            }
-        }
-        
     }
     
     func load(){
