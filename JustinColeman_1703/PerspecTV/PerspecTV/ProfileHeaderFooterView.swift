ae8ff0becaf9a4609f9fcd7621fd1f196340e28f
Justin <jcolethekid@gmail.com>
2017-03-10 06:40:19 -0500

    Created xib file for Profile Tableview Header

---
+++ JustinColeman_1703/PerspecTV/PerspecTV/ProfileHeaderFooterView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderFooterView.swift
+//  PerspecTV
+//
+//  Created by Justin Coleman on 3/10/17.
+//  Copyright © 2017 Justin Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
