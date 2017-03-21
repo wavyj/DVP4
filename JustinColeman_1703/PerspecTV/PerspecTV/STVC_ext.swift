f6deda1a03cb6f0a1541558e9b1ea8205b6d0656
Justin Coleman <Justin@Js-MacBook-Pro.local>
2017-03-21 03:00:54 -0400

    Added Selected Team View

---
+++ JustinColeman_1703/PerspecTV/PerspecTV/STVC_ext.swift
@@ -0,0 +1,16 @@
+//
+//  STVC_ext.swift
+//  PerspecTV
+//
+//  Created by Justin Coleman on 3/21/17.
+//  Copyright © 2017 Justin Coleman. All rights reserved.
+//
+
+import Foundation
+
+extension SelectedTeamViewController{
+    
+    func downloadAndParse(urlString: String){
+        
+    }
+}
